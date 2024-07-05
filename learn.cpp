2e4944daa6b51927628f2ad878ab36c29ea346f8
SanjayaSamudra <sanjayasamudraelpitiya@gmail.com>
2024-07-05 17:41:31 +0530

    first commit

--- learn.cpp
+++ learn.cpp
@@ -9,7 +9,31 @@ int main() {
 // Important facts regarding this simple code
 
 // 1. #include <iostream> 
-//			Refers to as Header file 
-//			This code gives the user to use input, output operations to carry out
+//			Refers to as Header file. 
+//			This code gives the user to use input, output operations to carry out.
+
+// 2. using namespace std;
+//			Use names for objects & variables from the standard library.
+//			Basically that means 'namespace' provide scope for identifiers (like variables, objects etc).
+//          Basically 'std' means standard, contains standard C++ library components.
+
+// A free newline given there is to make our code clean & nice. No specific task there.
+
+// 3. int main() {}
+//			Here 'int' means, return type. In this case it says to return integers.
+//			Here 'main()' refers to main function in the given code. Present of main function 'main()' is a must and you cannot change it.
+//			Here function body start with the opening bracket of curly brackets ({) and ends with closing bracket of curly bracket (}).   
+
+// 4. cout << "Hello World";
+//			Here 'cout; refers to character output.
+//			'<<' refers the insertion operator.
+//			So all together cout with insertion operator (<<) followed with double quotes ("") end with semicolon (;) is the complete code.
+//			Within the double quotes we are including what needs to be printed out.
+
+// 5. return 0;
+//			This confirms that main function executed successfully.
+//			Though above line is a statement, we have to end that line with semicolon(;).
+
+
 
 
