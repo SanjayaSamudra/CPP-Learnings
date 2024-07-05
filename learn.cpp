f53db3be332dc40b772f5942ecc0b8fb5253f0bc
SanjayaSamudra <sanjayasamudraelpitiya@gmail.com>
2024-07-05 15:05:06 +0530

    Initial commit

---
+++ learn.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World";
+	return 0;
+}
+
+// Important facts regarding this simple code
+
+// 1. #include <iostream> 
+//			Refers to as Header file 
+//			This code gives the user to use input, output operations to carry out
+
+
